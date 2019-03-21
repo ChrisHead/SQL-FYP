98805bbf290ad2e3a2fdd1573fb869470a60a012
Chris Head <chrishead90@hotmail.com>
2019-03-21 16:20:56 +0000

    production

--- web/deploy/server.js
+++ web/deploy/server.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const app = express()
+const fs = require("fs")
+fs.readdir(__dirname + "/../build/", (error, data) => {
+  console.log(error, data)
+})
 app.use(express.static(__dirname + "/../build"))
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/../build/index.html")
